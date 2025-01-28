4427c44065fcd3b6f1a03b4c932219d7e9b7cb65
IgorVotintsev <ivotints@student.42prague.com>
2025-01-28 13:54:35 +0100

    light move generation added

--- src/new_minimax.cpp
+++ src/new_minimax.cpp
@@ -1,10 +1,7 @@
 #include "gomoku.hpp"
 
-int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &total_evaluated, move_t *moves_last, short move_count_last, u_int64_t *TTable)
+inline void    light_move_generation(move_t *moves, short &move_count, move_t *moves_last, short move_count_last, move_t &move)
 {
-    move_t moves[300];
-    short move_count = 0;
-
     for (short i = 0; i < move_count_last; ++i) // rewrite all possible moves from last generation of moves.
     {
         if (moves_last[i].x == move.x && moves_last[i].y == move.y) // not to add current move
@@ -13,60 +10,33 @@ int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &to
         moves[move_count].y = moves_last[i].y;
         move_count++;
     }
-    // now we need to generate 8 moves around current move
 
     const char dir_vect[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}; //y, x //TODO can we put it into header? we have 3 copies of it. Repeating code...
-
+    
+    // now we need to generate 8 moves around current move
     for (uint8_t i = 0; i < 8; ++i)
     {
-        if  (   move.x + dir_vect[i][1] > 18
-            ||  move.x + dir_vect[i][1] < 0
-            ||  move.y + dir_vect[i][0] > 18
-            ||  move.y + dir_vect[i][0] < 0) // if move is not possible because it is out of bounce, we do not add it.
-            continue;
 
-        if (find_move(moves,  move.x + dir_vect[i][1], move.y + dir_vect[i][0], move_count)) // if move is in the list already, we do not add it.
+        int ny = move.y + dir_vect[i][0];
+        int nx = move.x + dir_vect[i][1];
+        if (nx < 0 || nx > 18 || ny < 0 || ny > 18) // out of map
             continue;
-        
-        if ((   (move.boards[0][move.y + dir_vect[i][0]] >> (move.x + dir_vect[i][1])) & 1)
-            || ((move.boards[1][move.y + dir_vect[i][0]] >> (move.x + dir_vect[i][1])) & 1)) // if move is already on board
+        if (((move.boards[0][ny] >> nx) & 1) || ((move.boards[1][ny] >> nx) & 1)) // if move is already on board
             continue;
-        moves[move_count].x = move.x + dir_vect[i][1];
-        moves[move_count].y = move.y + dir_vect[i][0];
-        move_count++;
+        if (find_move(moves, nx, ny, move_count)) // if move is in the list already
+            continue;
+        // we do not check for legality of move here, because later same move can become legal. We will check for legality before minimax call.
+        moves[move_count].y = ny;
+        moves[move_count].x = nx;
+        (move_count)++;
     }
 
-    // one more loop of 8 to generate moves around x and y but on the 2 level. It should fix problem with captures. Also model will be smarter, but it takes much more time.
-/*     for (uint8_t i = 0; i < 8; ++i)
-    {
-        if  (   move.x + 2 * dir_vect[i][1] > 18
-            ||  move.x + 2 * dir_vect[i][1] < 0
-            ||  move.y + 2 * dir_vect[i][0] > 18
-            ||  move.y + 2 * dir_vect[i][0] < 0)
-            continue;
-
-        if (find_move(moves,  move.x + 2 * dir_vect[i][1], move.y + 2 * dir_vect[i][0], move_count))
-            continue;
-        
-        if ((   move.boards[0][move.y + 2 * dir_vect[i][0]] >> (move.x + 2 * dir_vect[i][1]) & 1)
-            || (move.boards[1][move.y + 2 * dir_vect[i][0]] >> (move.x + 2 * dir_vect[i][1]) & 1))
-            continue;
-
-        if (!is_legal_lite(move.captures[turn], move.boards[turn], move.boards[!turn],  move.y + 2 * dir_vect[i][0], move.x + 2 * dir_vect[i][1]))
-            continue;
-        moves[move_count].x = move.x + 2 * dir_vect[i][1];
-        moves[move_count].y = move.y + 2 * dir_vect[i][0];
-        move_count++;
-    } */
-
-    // later we will check if there was capture before. If so, we will generate move on capture made.
-    if (move.capture_dir)
+    if (move.capture_dir) // if we had a capture, we should generate move
     { 
         for (uint8_t dir_index = 0; dir_index < 8; ++dir_index)
         {
             if (move.capture_dir & (1 << dir_index)) // check capture
             {
-                // if we had a capture, we should generate move there.
                 // it will be deffenetly not out of bounce,
                 // it will not be in list already
                 // there is obviously no piece in that place
@@ -76,32 +46,26 @@ int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &to
             }
         }
     }
+}
 
+int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &total_evaluated, move_t *moves_last, short move_count_last, u_int64_t *TTable)
+{
+    move_t moves[300];
+    short move_count = 0;
 
-    // and also we will run checker for double three 7x7 area with center in x and y. star pattern. 
-    
-
-
-
-
-
-
-
-
-
-
+    light_move_generation(moves, move_count, moves_last, move_count_last, move);
 
     for (short i = 0; i < move_count; ++i)
     {
         total_evaluated++;
         moves[i].eval = star_heuristic(move.boards, turn, move.captures, moves[i].y, moves[i].x, move.eval, moves[i].boards, moves[i].captures, moves[i].capture_dir);
         moves[i].hash = updateZobristHash(move.hash, moves[i].y, moves[i].x, turn, depth);
-        if (turn == BLACK) {
-            if (moves[i].eval > 100'000) // mean that it found winning move
-                return (moves[i].eval); }
-        else {
-            if (moves[i].eval < -100'000)
-                return (moves[i].eval); }
+        // if (turn == BLACK) {
+        //     if (moves[i].eval > 100'000) // mean that it found winning move
+        //         return (moves[i].eval); }
+        // else {
+        //     if (moves[i].eval < -100'000)
+        //         return (moves[i].eval); }
     }
     sort_moves(moves, move_count, turn); // now i need to sort them. i will call star_heuristc on every move and sort, depending on which turn is now. if turn is 0 that means we sort from biggrst to smallest.
 
@@ -119,7 +83,7 @@ int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &to
         best_eval = -1'000'000;
         for (short i = 0; i < move_count; ++i) // for move in moves
         {
-            if (!is_legal_lite(moves[i].captures[turn], moves[i].boards[turn], moves[i].boards[!turn], moves[i].y, moves[i].x) || isPositionVisited(TTable, moves[i].hash))
+            if (!is_legal_lite(moves[i].captures[turn], moves[i].boards[turn], moves[i].boards[!turn], moves[i].y, moves[i].x))// || isPositionVisited(TTable, moves[i].hash))
                 continue;
 
             int eval = moves[i].eval;
@@ -143,7 +107,7 @@ int new_minimax(move_t &move, bool turn, int alpha, int beta, int depth, int &to
         best_eval = 1'000'000;
         for (short i = 0; i < move_count; ++i)
         {
-            if (!is_legal_lite(moves[i].captures[turn], moves[i].boards[turn], moves[i].boards[!turn], moves[i].y, moves[i].x) || isPositionVisited(TTable, moves[i].hash))
+            if (!is_legal_lite(moves[i].captures[turn], moves[i].boards[turn], moves[i].boards[!turn], moves[i].y, moves[i].x))// || isPositionVisited(TTable, moves[i].hash))
                 continue;
 
             int eval = moves[i].eval;
@@ -230,16 +194,16 @@ BotResult new_bot_play(uint32_t (&boards)[2][19], bool turn, uint8_t (&captures)
         total_evaluated++;
         moves[i].eval = star_heuristic(boards, turn, captures, moves[i].y, moves[i].x, current_board_eval, moves[i].boards, moves[i].captures, moves[i].capture_dir);
         moves[i].hash = computeZobristHash(moves[i].boards[0], moves[i].boards[1], depth);
-        if (turn == BLACK) {
-            if (moves[i].eval > 100'000) {
-                delete[] TTable;
-                return {(moves[i].y * 19 + moves[i].x), moves[i].eval}; }
-            }
-        else {
-            if (moves[i].eval < -100'000) {
-                delete[] TTable;
-                return {(moves[i].y * 19 + moves[i].x), moves[i].eval}; }
-            }
+        // if (turn == BLACK) {
+        //     if (moves[i].eval > 100'000) {
+        //         delete[] TTable;
+        //         return {(moves[i].y * 19 + moves[i].x), moves[i].eval}; }
+        //     }
+        // else {
+        //     if (moves[i].eval < -100'000) {
+        //         delete[] TTable;
+        //         return {(moves[i].y * 19 + moves[i].x), moves[i].eval}; }
+        //     }
     }
     sort_moves(moves, move_count, turn);
 
